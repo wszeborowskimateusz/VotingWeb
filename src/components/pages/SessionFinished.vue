266e070a814cd295ebe6b90e7923c91aef593854
Mateusz <wszeborowskimateusz@gmail.com>
2020-10-27 21:19:18 +0100

    Adding confirmation dialog for session deletion

--- src/components/pages/SessionFinished.vue
+++ src/components/pages/SessionFinished.vue
@@ -5,13 +5,21 @@
       class="mb-5"
       dark
       v-if="activeSession.status === 'FINISHED'"
-      @click="removeSession(activeSession.id)"
+      @click="showConfirmationDialog = true"
     >
       <v-icon left>mdi-delete</v-icon>
       {{ $t('sessionActions.removeSession') }}
     </v-btn>
     <v-divider />
     <VotingsTabView :votingTypes="['FINISHED']" />
+    <ConfirmationDialog
+      :header="
+        $t('sessionActions.areYouSureToRemove', { sessionName: activeSession.name })
+      "
+      :description="$t('sessionActions.actionIsIrreversible')"
+      v-model="showConfirmationDialog"
+      @callback="removeSession(activeSession.id)"
+    />
   </div>
 </template>
 
@@ -20,6 +28,11 @@ import { mapGetters, mapActions } from 'vuex';
 import VotingsTabView from '../partials/votings/VotingsTabView.vue';
 
 export default {
+  data() {
+    return {
+      showConfirmationDialog: false,
+    };
+  },
   computed: {
     ...mapGetters('parliamentManagement', ['activeSession']),
   },
