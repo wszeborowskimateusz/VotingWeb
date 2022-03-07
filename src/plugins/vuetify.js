f839ae9613757f2f16ef9adf40f7d7751073318a
mateusz.wszeborowski <mateusz.wszeborowski@netguru.com>
2022-03-07 18:50:39 +0100

    Fixing icons offline

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,10 +1,15 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
 
+import '@mdi/font/css/materialdesignicons.css';
+
 Vue.use(Vuetify);
 
 export default new Vuetify({
   theme: {
     dark: true,
+  },
+  icons: {
+    iconfont: 'mdi',
   },
 });
